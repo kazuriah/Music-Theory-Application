2fc429fe0dd1bcde473ca2a367f7211f515d5d65
Christopher Hebert <hebert.christopherj@gmail.com>
2018-01-25 11:45:23 -0800

    incorporated react-simple-markdown into references

--- app-source-code/References.js
+++ app-source-code/References.js
@@ -1,7 +1,9 @@
 import { default as React, Component } from 'react';
 import { Platform, StyleSheet, Text, View, } from 'react-native';
 
+// For Left<-->Right page swiping
 import { default as Swiper } from 'react-native-swiper';
+import { default as Markdown } from 'react-native-simple-markdown';
 
 // f(component, props, childElements) => element
 const e = function(component, props, children) {
@@ -28,7 +30,7 @@ const eReferences =
    ]);
 
 // StyleSheet
-const styles = StyleSheet.create({
+const swipeStyles = StyleSheet.create({
   wrapper: {
   },
   slide1: {
@@ -54,25 +56,37 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
   }
-})
+});
 
 // f(style, text) => component
 const eSwipeView = function(style, text) {
   return e(View, {style: style}, [
-             e(Text, {style:styles.text}, text)
+             e(Text, {style:swipeStyles.text}, text),
           ]);
 }
 
 // component
 const cReferences = component({
   render: function() {
-    return e(Swiper, {style: styles.wrapper, showButtons: true}, [
-              eSwipeView(styles.slide1, 'Hello Swiper'),
-              eSwipeView(styles.slide2, 'Beautiful'),
-              eSwipeView(styles.slide3, 'And simple'),
+    return e(Swiper, {style: swipeStyles.wrapper, showButtons: true}, [
+              eMarkdown,
+              eSwipeView(swipeStyles.slide1, 'Hello Swiper'),
+              eSwipeView(swipeStyles.slide2, 'Beautiful'),
+              eSwipeView(swipeStyles.slide3, 'And simple'),
             ]);
   }
 })
+
+// text
+const markdownText =
+'#Markdown in react-native is so cool!\n\n' +
+'- You can **emphasize** what you want, or just _suggest it_.\n' +
+'- You can [link websites](https://twitter.com/Charles_Mangwa)\n' +
+'- You can add a Gif: \n\n' +
+'![Some GIF](https://media.giphy.com/media/dkGhBWE3SyzXW/giphy.gif)\n';
+
+// element
+const eMarkdown = e(Markdown, {}, [markdownText]);
 
 // component
 // accessed by the Main Menu sidebar
